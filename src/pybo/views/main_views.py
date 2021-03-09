45536beae06dfb77c0d7a121de15e34af93cb607
심승용 <seungyongshim@nexon.co.kr>
2021-03-09 17:49:49 +0900

    69p

--- src/pybo/views/main_views.py
+++ src/pybo/views/main_views.py
@@ -1,4 +1,6 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
+
+from pybo.models import Question
 
 bp = Blueprint("main", __name__, url_prefix="/")
 
@@ -7,6 +9,8 @@ bp = Blueprint("main", __name__, url_prefix="/")
 def hello():
     return "Hello, Pybo"
 
+
 @bp.route("/")
 def index():
-    return "Pybo, index"
+    question_list = Question.query.order_by(Question.create_date.desc())
+    return render_template("question/question_list.html", question_list=question_list)
